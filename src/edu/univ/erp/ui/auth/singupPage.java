d78a0776c8eb5573e085bc8c5b2068d6d3686d6f
PrabaljeetSingh0101 <prabaljeet24415@iiitd.ac.in>
2025-10-24 13:34:16 +0530

    testing lol

--- src/edu/univ/erp/ui/auth/singupPage.java
+++ src/edu/univ/erp/ui/auth/singupPage.java
@@ -11,7 +11,7 @@ import edu.univ.erp.auth.AuthService;
 
 public class singupPage {
     public static void main(String[] args) {
-        JFrame singupFrame = new JFrame("University ERP Singup"); 
+        JFrame singupFrame = new JFrame("University ERP SingupLOL"); 
 
         singupFrame.setSize(450, 300); 
         singupFrame.setLayout(null);
