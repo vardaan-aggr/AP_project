517d26acfd3a850f3402d468d124553869b8c298
vardaan-aggr <vardaan819@gmail.com>
2025-10-28 11:59:38 +0530

    password / security

--- src/edu/univ/erp/data/DatabaseConnector.java
+++ src/edu/univ/erp/data/DatabaseConnector.java
@@ -5,8 +5,7 @@ import java.sql.*;
 public class DatabaseConnector {
 
     private static final String DB_USER = "root"; 
-    private static final String DB_PASS = "mdo)&&*"; // <-- IMPORTANT: Change this!
-
+    private static final String DB_PASS = System.getenv("DB_PASS");
     // --- Connection Strings ---
     // These URLs tell JDBC where to find your databases
     // CORRECT URLS (using the mariadb format)
