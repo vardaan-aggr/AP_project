0bf2283b30307e0701925499058edb15a69c441d
vardaan-aggr <vardaan819@gmail.com>
2025-11-04 23:44:15 +0530

    feat: Add initial user seed data

---
+++ src/edu/univ/erp/data/seed_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO auth_table (roll_no, username, role, hash_password) VALUES
+    ('1', 'admin1', 'admin', '$2a$10$3b1FoH6INdCd/kR46yuG5OGJxTdTsd9Mdfk3c5mKbMTqgFT0Oa9qu'),
+    ('2', 'instructor1', 'instructor', '$2a$10$VIBCacj3YM50mKIhgugjq.LOUO3LO1tIkjhoKolBggMiLFKTN7Yfe'),
+    ('3', 'student1', 'student', '$2a$10$93zO4Dam2G0GeepgnQTZL.s1ao9saO64HbxqnSpkBZKOh4VgSKEsW')
+   
