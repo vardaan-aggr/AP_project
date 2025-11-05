867664c0c28e02fbab4d3a30377853e1e3627111
PrabaljeetSingh0101 <prabaljeet24415@iiitd.ac.in>
2025-11-05 13:15:35 +0530

    modifying login page actionlistner

---
+++ src/edu/univ/erp/ui/auth/LoginPage.java
@@ -0,0 +1,74 @@
+package edu.univ.erp.ui.auth;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import edu.univ.erp.data.DatabaseConnector;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;   
+import java.awt.event.ActionListener;
+
+public class LoginPage {
+
+    public static void main(String[] args) {
+        JFrame loginFrame = new JFrame("University ERP Login"); 
+
+        loginFrame.setSize(450, 300); 
+        loginFrame.setLayout(null);
+
+        JLabel userLabel = new JLabel("Username:");
+        userLabel.setBounds(50, 60, 100, 30);
+        loginFrame.add(userLabel);
+        
+        JTextField userText = new JTextField();
+        userText.setBounds(150, 60, 250, 30);
+        loginFrame.add(userText);
+        
+        JLabel passLabel = new JLabel("Password:");
+        passLabel.setBounds(50, 110, 100, 30);
+        loginFrame.add(passLabel);
+        
+        JPasswordField passText = new JPasswordField();
+        passText.setBounds(150, 110, 250, 30);
+        loginFrame.add(passText);
+        
+        JButton loginBtn = new JButton("Login");
+        loginBtn.setBounds(150, 170, 100, 30);
+        loginFrame.add(loginBtn);
+
+        loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        loginFrame.setLocationRelativeTo(null); 
+        loginFrame.setVisible(true);
+    
+    loginBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // 1. Get text from the fields
+                String username = userText.getText();
+                String password = new String(passText.getPassword());
+                try (Connection connection = DatabaseConnector.getAuthConnection()) {
+                    try (PreparedStatement statement = connection.prepareStatement("""
+                                Select hash_password FROM auth_table WHERE username = '?'; 
+                            """)) {
+                        statement.setString(1, username);
+                        try (ResultSet resultSet = statement.executeQuery()) {
+                            boolean empty = true;
+                            while (resultSet.next()) {
+                                empty = false;
+                                String hash_password = resultSet.getString("hash_password");
+                                System.out.println("Billi kre meow meow 🙀: "+hash_password);
+                            }
+                            if (empty) {
+                                System.out.println("\t (no data)");
+                            }
+                        }
+                    } 
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+    }
+}
