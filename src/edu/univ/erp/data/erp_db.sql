40cc9457ef516d56f78a4ea8d82ca9c343bc9e20
vardaan-aggr <vardaan819@gmail.com>
2025-11-09 15:30:45 +0530

    instructor progressed

--- src/edu/univ/erp/data/erp_db.sql
+++ src/edu/univ/erp/data/erp_db.sql
@@ -2,40 +2,44 @@ Create database IF NOT EXISTS erp_db;
 use erp_db;
 
 Create table IF NOT EXISTS students (
-    roll_no int,
+    roll_no INT PRIMARY KEY,
     program varchar(50),
-    year int
+    year INT 
 );
 Create table IF NOT EXISTS instructors (
-    roll_no int,
+    roll_no INT PRIMARY KEY,
     department varchar(50)
 );
 Create table IF NOT EXISTS courses (
     course_code varchar(50),
     title varchar (50),
     section varchar(50),
-    credits int
+    credits int 
 );
 Create table IF NOT EXISTS enrollments (
-    roll_no int,
+    roll_no INT ,
     section_id varchar(50),
     status varchar(50)
 );
 Create table IF NOT EXISTS sections (
     course_code varchar(50),
-    roll_no varchar(50),
+    
+    roll_no INT ,       -- instructor id
     day_time varchar (50),
     room varchar (50),
-    capacity int,
+    capacity INT  ,
     semester varchar(50),
-    year int
+    year INT 
 );
 Create table IF NOT EXISTS grades (
-    roll_no int,
+    roll_no INT ,
     course_code varchar(50),
-    -- section varchar(50),
+    section varchar(50),
     grade varchar(50)
 );
+
+
+ 
 -- Create table IF NOT EXISTS settings (
 --     students varchar(50),
 --     instructor role (50),
