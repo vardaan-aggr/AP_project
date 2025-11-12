f9c2d9ded8ee4d383484ed279e7dc629d6616514
vardaan-aggr <vardaan819@gmail.com>
2025-11-12 22:50:59 +0530

    resolve

--- src/edu/univ/erp/data/seed_data.sql
+++ src/edu/univ/erp/data/seed_data.sql
@@ -1,11 +1,20 @@
-INSERT INTO auth_table (roll_no, username, role, hash_password) VALUES
+use auth_db;
+use erp_db;
+TRUNCATE TABLE auth_db.auth_table;
+TRUNCATE TABLE courses;
+TRUNCATE TABLE instructors;
+TRUNCATE TABLE enrollments;
+TRUNCATE TABLE grades;
+TRUNCATE TABLE sections;
+
+INSERT INTO auth_table  (roll_no, username, role, hash_password) VALUES
     ('1', 'admin1', 'admin', '$2a$10$3b1FoH6INdCd/kR46yuG5OGJxTdTsd9Mdfk3c5mKbMTqgFT0Oa9qu'),
     ('2', 'instructor1', 'instructor', '$2a$10$VIBCacj3YM50mKIhgugjq.LOUO3LO1tIkjhoKolBggMiLFKTN7Yfe'),
-    ('3', 'student1', 'student', '$2a$10$p.uYMyUofLa94cKp0Lqfsuwm.RaYPRKBYAUtGpycR/aPXSUgo7u/S')
-Insert into courses (course_code, title, section, credits) VALUES 
-    ('ECE250', 'Signals and Systems', 'N', '4')
+    ('3', 'student1', 'student', '$2a$10$p.uYMyUofLa94cKp0Lqfsuwm.RaYPRKBYAUtGpycR/aPXSUgo7u/S');
+Insert into courses(course_code, title, section, credits) VALUES 
+    ('ECE250', 'Signals and Systems', 'N', '4');
 use erp_db;
-Insert into instructors (roll_no, department) VALUES
+Insert  into instructors (roll_no, department) VALUES
     ('2','ECE250');
 Insert into enrollments (roll_no, course_code ,section, status) values 
     ('3', 'ECE250', 'N', 'enrolled');
@@ -13,6 +22,6 @@ Insert into grades (roll_no, course_code, section, grade) values
     ('3', 'ECE250', 'N', 'A');
 use erp_db;
 Insert into sections (course_code, section, roll_no, day_time, room, capacity, semester, year) values
-    ('ECE250', 'N', '2', 'Wed 10:30-12:00, Fri 8:30-11:00', 'C102', '600', 'Monsoon', '2025')
+    ('ECE250', 'N', '2', 'Wed 10:30-12:00, Fri 8:30-11:00', 'C102', '600', 'Monsoon', '2025');
 use erp_db;
 select * from grades;
